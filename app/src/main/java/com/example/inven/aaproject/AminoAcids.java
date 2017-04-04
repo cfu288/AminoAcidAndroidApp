c7af358d42997cf37e410da42ca91371b345dafd
Chris F <Chris F>
2017-04-04 04:53:21 -0400

    Removed some padding

---
+++ app/src/main/java/com/example/inven/aaproject/AminoAcids.java
@@ -0,0 +1,22 @@
+package com.example.inven.aaproject;
+
+/**
+ * Created by inven on 4/3/2017.
+ */
+
+public class AminoAcids {
+    public static final int[] aaArr = {
+            R.drawable.alanine, R.drawable.arginine, R.drawable.asparagine,
+            R.drawable.asparticacid, R.drawable.cystine, R.drawable.glutamicacid,
+            R.drawable.glutamine, R.drawable.glycine, R.drawable.histidine,
+            R.drawable.isoleucine,R.drawable.leucine, R.drawable.lysine,
+            R.drawable.methionine, R.drawable.phenylalanine, R.drawable.proline,
+            R.drawable.serine, R.drawable.threonine, R.drawable.tryptophan,
+            R.drawable.tyrosine, R.drawable.valine
+    };
+    public static final String[] aaStrArr = {"Alanine","Arginine","Asparagine","Aspartic Acid","Cysteine",
+            "Glutamic Acid","Glutamine","Glycine","Histidine","Isoleucine","Leucine",
+            "Lysine","Methionine","Phenylalanine","Proline","Serine","Threonine",
+            "Tryptophan","Tyrosine","Valine"
+    };
+}
